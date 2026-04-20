ceba736d3d9666d3e7d89a06fbb67e84ffa00c13
Tarek Elsayed <tarekkalsayed@gmail.com>
2026-04-20 05:05:12 +0200

    Add validation logic and user templates

--- src/main/java/org/example/service/Validation.java
+++ src/main/java/org/example/service/Validation.java
@@ -1,4 +1,36 @@
 package org.example.service;
 
+import org.example.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Validation {
+
+    public boolean validateAdminLogin(User user) {
+        if ("admin".equals(user.getUsername()) &&
+                "12345".equals(user.getPassword())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean validateUser(User user) {
+        if (user.getUsertype().isEmpty() || !(
+                user.getUsertype().equals("user")
+                        || user.getUsertype().equals("admin")
+        )) {
+            return false;
+        }
+
+        if (user.getUsername().isEmpty() || user.getUsername().length() < 4) {
+            return false;
+        }
+
+        if (user.getPassword().isEmpty() || user.getPassword().length() <= 5) {
+            return false;
+        }
+
+        return true;
+    }
 }
